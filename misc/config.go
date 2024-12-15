2450aad59d448b9484d697586138cb75abb84c2c
Yiffyi Jia <git@yif.fyi>
2024-12-15 13:12:23 +0000

    Revert "update: config use camel case"
    This reverts commit 4f2cd3ad5d389ba26c1f7bd89295d0af532d8752.

--- misc/config.go
+++ misc/config.go
@@ -13,23 +13,23 @@ func setupViper() {
 
 	viper.SetDefault("log.path", "/var/log/bb.log")
 	viper.SetDefault("log.console", true)
-	viper.SetDefault("log.pamDebugDump", false)
+	viper.SetDefault("log.pam_debug_dump", false)
 
 	viper.SetDefault("push.channel", "telegram")
 	viper.SetDefault("push.telegram.token", "")
-	viper.SetDefault("push.telegram.toUsername", "")
-	viper.SetDefault("push.telegram.toChatID", 0)
+	viper.SetDefault("push.telegram.to_username", "")
+	viper.SetDefault("push.telegram.to_chatid", 0)
 	viper.SetDefault("push.telegram.timeout", time.Duration(5)*time.Second)
 
 	viper.SetDefault("honeypot.enabled", false)
-	viper.SetDefault("honeypot.serverVersion", "SSH-2.0-OpenSSH_8.4p1 Debian-5+deb11u3")
-	viper.SetDefault("honeypot.serverHostKeys", []string{"id_rsa"})
-	viper.SetDefault("honeypot.listenAddrs", []string{"0.0.0.0:2022"})
-	viper.SetDefault("honeypot.allowAnyCreds", false)
+	viper.SetDefault("honeypot.server_version", "SSH-2.0-OpenSSH_8.4p1 Debian-5+deb11u3")
+	viper.SetDefault("honeypot.server_host_keys", []string{"id_rsa"})
+	viper.SetDefault("honeypot.listen_addrs", []string{"0.0.0.0:2022"})
+	viper.SetDefault("honeypot.allow_any_creds", false)
 
-	viper.SetDefault("installer.honeypotPath", "/usr/local/bin/honeypot")
-	viper.SetDefault("installer.honeypotServiceUnit", "/etc/systemd/system/bb-honeypot.service")
-	viper.SetDefault("installer.pamPath", "/usr/local/lib/pam_bb.so")
+	viper.SetDefault("installer.honeypot_exe", "/usr/local/bin/honeypot")
+	viper.SetDefault("installer.honeypot_service_unit", "/etc/systemd/system/bb-honeypot.service")
+	viper.SetDefault("installer.pam_bb_exe", "/usr/local/lib/pam_bb.so")
 }
 
 func LoadConfig() error {
