c8771351f0692e4b8d6e59ce3b5e7b8539287f56
Yiffyi Jia <git@yif.fyi>
2024-12-15 09:05:00 +0000

    fix: build-pam.sh package path

--- build-pam.sh
+++ build-pam.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-CGO_CFLAGS="-O2" go build -v -trimpath -ldflags="-s -w" --buildmode=c-shared -o pam_bb.so ./pam_bb
+CGO_CFLAGS="-O2" go build -v -trimpath -ldflags="-s -w" --buildmode=c-shared -o bin/pam_bb.so ./exe/pam_bb
