2dd6938016bd6577a7e8476ccde36f2af4f1059b
Yiffyi Jia <git@yif.fyi>
2024-12-16 02:49:09 +0000

    fix: expose build error in build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 export CGO_CFLAGS="-Os"
 
 echo "-- [ Building exe/pam_bb ] --"
@@ -9,7 +9,8 @@ echo "-- [ Building exe/honeypot ] --"
 go build -v -trimpath -ldflags="-s -w" -o bin/honeypot ./exe/honeypot
 
 # don't pack main executable
-rm bin/bb exe/bb/install.tar exe/bb/install.tar.zst
+# it's OK that those doesn't exists
+rm bin/bb exe/bb/install.tar exe/bb/install.tar.zst || true
 
 tar -cf exe/bb/install.tar -C bin .
 zstd -9 exe/bb/install.tar -o exe/bb/install.tar.zst
