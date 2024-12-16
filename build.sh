efd3b433940abb24db05de10f7b17579259015ce
Yiffyi Jia <git@yif.fyi>
2024-12-16 02:47:40 +0000

    fix: install dependency; better build.sh output

--- build.sh
+++ build.sh
@@ -2,7 +2,10 @@
 
 export CGO_CFLAGS="-Os"
 
+echo "-- [ Building exe/pam_bb ] --"
 go build -v -trimpath -ldflags="-s -w" --buildmode=c-shared -o bin/pam_bb.so ./exe/pam_bb
+
+echo "-- [ Building exe/honeypot ] --"
 go build -v -trimpath -ldflags="-s -w" -o bin/honeypot ./exe/honeypot
 
 # don't pack main executable
@@ -11,4 +14,5 @@ rm bin/bb exe/bb/install.tar exe/bb/install.tar.zst
 tar -cf exe/bb/install.tar -C bin .
 zstd -9 exe/bb/install.tar -o exe/bb/install.tar.zst
 
+echo "-- [ Building exe/bb ] --"
 go build -v -trimpath -ldflags="-s -w" -o bin/bb ./exe/bb
