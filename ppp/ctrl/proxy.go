c75a057714db0f8fa685619d9bf52ab0db127034
Yiffyi Jia <git@yif.fyi>
2025-08-11 01:45:37 +0800

    add: generate server config

--- ppp/ctrl/proxy.go
+++ ppp/ctrl/proxy.go
@@ -27,9 +27,10 @@ type Hysteria2SupplementInfo struct {
 	down          int
 	tls           bool
 	tlsServerName string
+	acmeEmail     string
 }
 
-func (s *Hysteria2SupplementInfo) singBox() map[string]any {
+func (s *Hysteria2SupplementInfo) singBoxClient() map[string]any {
 	return map[string]any{
 		"password":  s.password,
 		"up_mbps":   s.up,
@@ -41,6 +42,30 @@ func (s *Hysteria2SupplementInfo) singBox() map[string]any {
 	}
 }
 
+func (s *Hysteria2SupplementInfo) singBoxServer() map[string]any {
+	return map[string]any{
+		"users": []map[string]any{
+			{
+				"name":     "sekai",
+				"password": s.password,
+			},
+		},
+		"up_mbps":                 s.up,
+		"down_mbps":               s.down,
+		"ignore_client_bandwidth": false,
+		"tls": map[string]any{
+			"enabled":     s.tls,
+			"server_name": s.tlsServerName,
+			"acme": map[string]any{
+				"domain": s.tlsServerName,
+				"email":  s.acmeEmail,
+			},
+			"alpn": []string{
+				"h3",
+			},
+		},
+	}
+}
 func (s *Hysteria2SupplementInfo) clash() map[string]any {
 	return map[string]any{
 		"tls":  s.tls,
@@ -67,24 +92,35 @@ func (s *Hysteria2SupplementInfo) SpecializeClientConfig(clientType model.Progra
 }
 
 func (s *Hysteria2SupplementInfo) SpecializeServerConfig(serverType model.ProgramType, genericInfo map[string]any) (map[string]any, error) {
-	panic("unsupported yet")
+	var supp map[string]any
+	switch serverType {
+	case model.PROGRAM_TYPE_SINGBOX:
+		supp = s.singBoxServer()
+	default:
+		return nil, errors.New("unsupported server type")
+	}
+
+	maps.Copy(genericInfo, supp)
+
+	return genericInfo, nil
 }
 
 type VmessSupplementInfo struct {
-	uuid             string
-	security         string
-	alterId          int
-	tls              bool
-	tlsServerName    string
-	utls             bool
-	utlsFingerprint  string
-	reality          bool
-	realityPublicKey string
-	realityShortId   string
-	multiplex        bool
+	uuid              string
+	security          string
+	alterId           int
+	tls               bool
+	tlsServerName     string
+	utls              bool
+	utlsFingerprint   string
+	reality           bool
+	realityPrivateKey string
+	realityPublicKey  string
+	realityShortId    string
+	multiplex         bool
 }
 
-func (s *VmessSupplementInfo) singBox() map[string]any {
+func (s *VmessSupplementInfo) singBoxClient() map[string]any {
 	return map[string]any{
 		"uuid":     s.uuid,
 		"security": s.security,
@@ -105,6 +141,37 @@ func (s *VmessSupplementInfo) singBox() map[string]any {
 		"multiplex": map[string]any{
 			"enabled":     s.multiplex,
 			"max_streams": 3,
+		},
+	}
+}
+
+func (s *VmessSupplementInfo) singBoxServer() map[string]any {
+	return map[string]any{
+		"users": []map[string]any{
+			{
+				"name":    "sekai",
+				"uuid":    s.uuid,
+				"alterId": s.alterId,
+			},
+		},
+		"tls": map[string]any{
+			"enabled":     s.tls,
+			"server_name": s.tlsServerName,
+			"alpn": []string{
+				"h2", "http/1.1", "h3",
+			},
+			"reality": map[string]any{
+				"enabled": s.reality,
+				"handshake": map[string]any{
+					"server":      s.tlsServerName,
+					"server_port": 443,
+				},
+				"private_key": s.realityPrivateKey,
+				"short_id":    s.realityShortId,
+			},
+		},
+		"multiplex": map[string]any{
+			"enabled": s.multiplex,
 		},
 	}
 }
@@ -135,7 +202,7 @@ func (s *VmessSupplementInfo) SpecializeClientConfig(clientType model.ProgramTyp
 	var supp map[string]any
 	switch clientType {
 	case model.PROGRAM_TYPE_SINGBOX:
-		supp = s.singBox()
+		supp = s.singBoxClient()
 	case model.PROGRAM_TYPE_CLASH:
 		supp = s.clash()
 	default:
@@ -148,5 +215,15 @@ func (s *VmessSupplementInfo) SpecializeClientConfig(clientType model.ProgramTyp
 }
 
 func (s *VmessSupplementInfo) SpecializeServerConfig(serverType model.ProgramType, genericInfo map[string]any) (map[string]any, error) {
-	panic("unsupported yet")
+	var supp map[string]any
+	switch serverType {
+	case model.PROGRAM_TYPE_SINGBOX:
+		supp = s.singBoxServer()
+	default:
+		return nil, errors.New("unsupported server type")
+	}
+
+	maps.Copy(genericInfo, supp)
+
+	return genericInfo, nil
 }
